bc3272ea8b2943491d29dbd87b409933c8e54fe8
chenweishan <3111594864@qq.com>
2017-06-01 19:30:10 +0800

    再次修改
    再次修改专题内容

--- pick_a_National GDP_4web/pick_a_National GDP_4web.py
+++ pick_a_National GDP_4web/pick_a_National GDP_4web.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def entry_page() -> 'html':
     """Display this webapp's HTML form."""
     return render_template('entry.html',
-                           the_title='欢迎来到网上选首都！')
+                           the_title='欢迎来到网上选国家GDP！')
 
-@app.route('/pick_a_capital', methods=['POST'])
+@app.route('/pick_a_national_GDP', methods=['POST'])
 def pick_a_capital() -> 'html':
     """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
-    user_capital = request.form['user_capital']	
+    user_country_GDP = request.form['user_capital']	
     return render_template('results.html',
-                           the_title = '以下是您选取的手都：',
-                           the_capital = user_capital,
+                           the_title = '以下是您选取的国家GDP：',
+                           the_country_GDP = user_country_GDP,
                            )
 
 if __name__ == '__main__':
